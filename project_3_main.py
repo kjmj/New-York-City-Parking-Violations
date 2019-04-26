324200da9d3caf48c4d69cc64bd56b0dfd3a1312
Ken Morton Jr <kmorton15@gmail.com>
2019-04-26 00:44:54 -0400

    optimized our script for pandas

--- project_3_main.py
+++ project_3_main.py
@@ -1,13 +1,9 @@
 import pandas as pd
 
-
+# read in our csv
 df = pd.read_csv('data/csv/ParkingViolations2015-Partial.csv', nrows=10000)
 
-columnHeaders = list(df.columns.values)
-print(columnHeaders)
-
-countyList = []
-valueDictionary = {}
+# define map from county code to full name
 countyTranslations = {
      'MN':'Manhattan',
      'NY':'Manhattan',
@@ -19,71 +15,13 @@ countyTranslations = {
      'BK': 'Brooklyn',
      'BX':'Bronx',
      'nan':'null'
-     }
+}
 
-for index, row in df.iterrows():
-     county = row["Violation County"]
-     
-     if county not in countyList:
-          if not isinstance(county, str):
-               countyList.append(str(county))
-          else:
-               countyList.append(county)
-                        
-     if county not in valueDictionary:
-          valueDictionary[str(county)] = 1;
-     else:
-          currentValue = valueDictionary[str(county)];
-          valueDictionary[str(county)] = currentValue + 1;
+# print out our column headers
+columnHeaders = list(df.columns.values)
+print(columnHeaders)
 
-print("RESULTS:");
-for key in valueDictionary:
-     print(key, ": ", valueDictionary[key])
-
-#print('##############')
-#for key in countyTranslations:
- #    print(key, ": ", countyTranslations[key])
-combinedValuesDictionary = {}
-
-print("PROCESSING/COMBINING RESULTS....")
-for key in valueDictionary:
-     #if countyTranslations[key] != 'nan':
-     included = False;
-     for key2 in valueDictionary:
-         # print(key2)
-          #print(countyTranslations[key2])
-          
-          if countyTranslations[key] == countyTranslations[key2]:
-               if key != key2:
-                    #true duplicate
-                    trueValue = valueDictionary[key] + valueDictionary[key2]
-                    combinedValuesDictionary[countyTranslations[key]] = trueValue
-                    included = True
-               #combinedValuesDictionary[key] = trueValue; 
-               
-               #flag duplicate and add to results dictionary with the countryTranslations[key] as the key
-               print("duplicate found")
-                         #combine
-                              
-     if not included:
-          combinedValuesDictionary[countyTranslations[key]] = valueDictionary[key]
-                         
-
-print("COMBINED/PROCESSED VALUES:");
-for key in combinedValuesDictionary:
-     print(key, ": ", combinedValuesDictionary[key])
-                         
-#for row in df.itertuples(index=True, name='Pandas'):
- #    getattr(row, "Registration State") 
-  #   county = getattr(row, "Violation County")
-   #  print(county)
-    
-    
-    
-    
-    
-    
-
-
-
-
+df = pd.DataFrame(df, columns=['Violation County'])  # consider only the violation county column
+df = df.groupby('Violation County')['Violation County'].count().reset_index(name='count')  # group by violation county, count unique, add count column
+df['Full Name'] = df['Violation County'].map(countyTranslations)  # map from county code to full name
+print(df)
